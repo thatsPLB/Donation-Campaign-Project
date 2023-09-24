46d8847f871db68d93d09a3b1b43a344899d358b
Plabon Saha <sahaplabon66@gmail.com>
2023-09-25 02:02:59 +0600

    Tailwind,DaisyUi,react route json setup done

---
+++ src/Router/Route.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const myCreatedRoute = createBrowserRouter([
+    {
+        path:"/",
+        element: 
+    }
+])
+
+export default Route;
